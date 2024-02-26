d5b6cf61456c7794c1ba7cc3b6c4b87f888c2a28
Leonardo Mendes <lsm.ccomp@gmail.com>
2024-02-25 22:24:16 -0300

    update to test vercel

---
+++ api/index.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, Authorization, accept, origin, token")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		c.Next()
+	}
+}
+
+func Handler(c *gin.Context) {
+	gin.SetMode(gin.ReleaseMode)
+	app := config.App()
+	env := app.Env
+	gin := gin.Default()
+	gin.Use(CORSMiddleware())
+	db := app.Db
+	route.Setup(env, db, gin)
+	gin.ServeHTTP(c.Writer, c.Request)
+}
